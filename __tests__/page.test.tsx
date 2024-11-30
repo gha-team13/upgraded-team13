a44e7bd9042ade15f87f02a79c16ffec70536ce5
tech jam <adcost68@gmail.com>
2024-11-29 22:13:26 -0500

    Add tests

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -19,7 +19,7 @@ describe("Home", () => {
     render(await Home());
 
     const docH = screen.getByRole("heading", {
-      name: "Hello World",
+      name: "Hello TechJam2024",
       level: 2,
     });
 
